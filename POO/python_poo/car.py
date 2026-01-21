5e5c58e69a15e6d290696844b5e7f6f042e028ea
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-01-21 11:44:54 -0300

    ok

--- POO/python_poo/car.py
+++
@@ -1,6 +0,0 @@
-
-from veiculo import Veiculo
-class Car(Veiculo):
-    def __init__(self, model, year, color, for_sale, vei_type):
-        super().__init__(model, year, color, for_sale)
-        self.vei_type = vei_type
--- POO/python_poo/veiculo.py
+++
@@ -1,8 +0,0 @@
-
-class Veiculo:
-    def __init__(self, model, year, color, for_sale):
-        self.model = model 
-        self.year = year 
-        self.color = color
-        self.for_sale = for_sale
-    
